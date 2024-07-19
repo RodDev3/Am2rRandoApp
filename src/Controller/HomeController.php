6756f29ca2dfa433ae8e283f70d873d6886d936c
Rodo-Gala <Rodolphe.davidg@gmail.com>
2024-07-20 00:05:40 +0200

    First test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,8 +17,8 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        while(true) {
-            echo "Hello World";
+        for ($i = 0; $i < 10; $i++) {
+            echo $i;
         }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/home');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('h1', 'Welcome');
+    }
+
+}
