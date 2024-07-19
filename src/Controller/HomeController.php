8b85b8f9cb0fcfb7ee705840827dee7ef73235ac
Rodo-Gala <Rodolphe.davidg@gmail.com>
2024-07-19 23:43:58 +0200

    Fix comments

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,8 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+/**
+ * Class HomeController
+ */
 class HomeController extends AbstractController
 {
+    /**
+     * Route for home page
+     */
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
